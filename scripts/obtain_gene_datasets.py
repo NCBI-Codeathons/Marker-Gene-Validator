53547e4b00cf843c1b6acf281e7116b8cb450250
tufanotl <tufanotl@ncbi.nlm.nih.gov>
2020-07-10 13:39:16 -0400

    add logging

--- scripts/obtain_gene_datasets.py
+++ scripts/obtain_gene_datasets.py
@@ -1,9 +1,13 @@
 import argparse
+import logging
 import json
 import subprocess
 from Bio import Entrez
 
 gene_ids_file = "gene_ids.txt"
+
+logging.basicConfig(filename='process_markers.log', level=logging.DEBUG)
+logger = logging.getLogger(f"{__file__}:{__name__}")
 
 def main():
     parser = argparse.ArgumentParser()
@@ -18,6 +22,12 @@ def main():
     json_data = format_file_data_into_json(gene_ids_file)
     obtain_gene_datasets(json_data, args.output_file)
     
+def log_and_raise_exception(error_message):
+    """input: error_message (error message string)
+       logs error and raises exception
+    """ 
+    logger.error(error_message)
+    raise Exception(error_message)
     
 def populate_gene_ids_file(query, email, gene_ids_file):
     """input: query (query to give esearch), email (email to provide biopython),
@@ -30,8 +40,11 @@ def populate_gene_ids_file(query, email, gene_ids_file):
     ids = esearch_record["IdList"]
     if total_record_number > len(ids):
         ids = Entrez.read(Entrez.esearch(db="gene", term=query, retmax=total_record_number))["IdList"]
+    result_count_message = f"Gene search for query '{query}' returned {total_record_number} results"
+    print(result_count_message)
+    logger.info(result_count_message)
     if len(ids) == 0:
-        raise Exception("The provided search query gave yielded no results in esearch")
+        log_and_raise_exception("The provided search query gave yielded no results in esearch")
     with open(gene_ids_file, "w+") as f:
         for result in ids:
             f.write(f"{result}\n")
@@ -44,8 +57,7 @@ def obtain_gene_datasets(gene_json, output_file):
     # Fix later to not use curl and handle non-200 responses
     return_code = subprocess.check_call(f'curl -X POST "https://api.ncbi.nlm.nih.gov/datasets/v1alpha/download/gene?filename=ncbi_dataset.zip" -H "accept: application/zip" -H "Content-Type: application/json" --data \'{gene_json}\' > {output_file}', shell=True)
     if return_code != 0:
-        raise Exception("Command to obtain datasets failed")
-
+        log_and_raise_exception("The provided search query gave yielded no results in esearch")
 
 def format_file_data_into_json(data_file):
     """input: data_file (sequence ids separated by \n)
